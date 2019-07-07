aeec18fb1d0ff7298fd08d669ff14fb625303109
= <robertsorange@gmail.com>
2019-07-07 11:07:18 -0600

    Added stats

--- src/services/Interfaces.d.ts
+++ src/services/Interfaces.d.ts
@@ -3,3 +3,30 @@ export interface Member {
 	membershipType: number;
 	displayName: string;
 };
+
+export interface Stats {
+	// Response.mergedAllCharacters.results
+	pve: { //allPvE.allTime
+		activitiesCleared: number; //activitiesCleared.basic.value
+		assists: number; //assists.basic.value
+		kills: number; //kills.basic.value
+		timePlayed: string; //secondsPlayed.basic.displayValue
+		deaths: number; //deaths.basic.value
+		kdRatio: string; //killsDeathsRatio.basic.displayValue
+		publicEventsCompleted: number; //publicEventsCompleted.basic.value
+	};
+	pvp: { //allPvP.allTime
+		activitesWon: number; //activitesWon.basic.value
+		assists: number; //assists.basic.value
+		kills: number; //kills.basic.value
+		timePlayed: string; //secondPlayed.basic.displayValue
+		deaths: number; //deaths.basic.value
+		bestSingleGameKils: number; //bestSingleGameKills.basic.value
+		opponentsDefeated: number; //opponentsDefeated.basic.value
+		efficiency: string; //efficiency.basic.displayValue
+		kdRatio: string; //killsDeathsRatio.basic.displayValue
+		winLossRatio: string; //winLossRatio.basic.displayValue
+		longestKillStreak: number; //longestKillSpree.basic.value
+
+	};
+}
