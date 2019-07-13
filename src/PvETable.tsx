9dae355d7e9fc48e9f7353c84c94e2b1c3c85d2e
= <robertsorange@gmail.com>
2019-07-12 20:10:25 -0600

    Converting sort to lower case

--- src/PvETable.tsx
+++ src/PvETable.tsx
@@ -1,34 +1,88 @@
 import React from 'react';
 import { Table } from "semantic-ui-react";
 import { Member, Stats } from "./services/Interfaces";
+import _ from 'lodash';
 
 interface PvETableProps {
 	members: Member[];
 	stats: Stats[];
 }
 
-export default class PvETable extends React.Component<PvETableProps, {}> {
+interface PvETableState {
+	column: any;
+	data: temp[];
+	direction: any;
+}
+
+interface temp {
+	name: string;
+	timePlayed: string;
+	kdRatio: string;
+}
+
+export default class PvETable extends React.Component<PvETableProps, PvETableState> {
+
+	constructor(props: any) {
+		super(props);
+
+		this.state = {
+			column: null,
+			data: this.props.members.map((member, index) => {
+				return {
+					'name': member.displayName,
+					'timePlayed': this.props.stats[index].pve!.timePlayed,
+					'kdRatio': this.props.stats[index].pve!.kdRatio,
+				};
+			}),
+			direction: null,
+		};
+	}
+
+	// Double function here to make sure that set state isn't called continously
+	handleSort = (clickedColumn: string) => () => {
+
+		const { column, data, direction } = this.state;
+
+		if(column !== clickedColumn) {
+			this.setState({
+				column: clickedColumn,
+				data: _.sortBy(data, [(item: any) => item[clickedColumn].toLowerCase()]),
+				direction: 'ascending'
+			});
+			
+			return;
+		}
+
+		this.setState({
+			data: data.reverse(),
+			direction: direction === 'ascending' ? 'descending' : 'ascending',
+		});
+
+	}
 
 	render() {
+		const { column, data, direction } = this.state;
+
 		return (
-			<Table celled>
+			<Table sortable celled>
 				<Table.Header>
 					<Table.Row>
-						<Table.HeaderCell>
+						<Table.HeaderCell sorted={column === 'name' ? direction : null} onClick={this.handleSort('name')}>
 							Name
 						</Table.HeaderCell>
-						<Table.HeaderCell>
+						<Table.HeaderCell sorted={column === 'timePlayed' ? direction : null} onClick={this.handleSort('timePlayed')}>
 							Time Played
 						</Table.HeaderCell>
 					</Table.Row>
 				</Table.Header>
 
 				<Table.Body>
-					{this.props.members.map((member, index) => {
+					{
+						data.map((member, index) => {
 						return (
-							<Table.Row key={member.displayName}>
-								<Table.Cell>{member.displayName}</Table.Cell>
-								<Table.Cell>{this.props.stats[index].pve!.timePlayed}</Table.Cell>
+							<Table.Row key={member.kdRatio}>
+								<Table.Cell>{member.name}</Table.Cell>
+								<Table.Cell>{member.timePlayed}</Table.Cell>
 							</Table.Row>
 						);
 					})}
