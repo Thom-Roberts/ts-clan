bdfcbcec9f28aa3461ead2f6518a1ce396734936
Thomas Roberts <robertsorange@gmail.com>
2019-07-26 07:23:43 -0600

    Added some styles

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,22 +7,29 @@ interface HomeProps {
 }
 
 export default function Home(props: HomeProps) {
+	const { name, creationDate, about, motto, founder } = props.Info;
 	return (
 		<div>
-			<img src={clanBanner} alt={props.Info.name} style={{width: '50px'}}/>
+			<img src={clanBanner} alt={name} style={{width: '50px'}}/>
+			<h3 style={{marginBottom: '0px',}}>{name} <span style={{fontSize: '12px', color: 'gray',}}>Circa {creationDate.getMonth() + 1}/{creationDate.getDate()}/{creationDate.getFullYear()}</span> </h3>
+			<h6 style={{marginTop: '0px',}}>{motto}</h6>
+			<p>
+				{about}
+			</p>
+			
 			<ul>
-				<li>Name: {props.Info.name}</li>
-				<li>Creation Date: {props.Info.creationDate.toString()}</li>
-				<li>About: {props.Info.about}</li>
-				<li>Motto: {props.Info.motto}</li>
+				<li>Name: {name}</li>
+				<li>Creation Date: {creationDate.toString()}</li>
+				<li>About: {about}</li>
+				<li>Motto: {motto}</li>
 				<li>
 					Founder:
 					<ul>
 						<li>
-							Display name: {props.Info.founder.destinyInfo.displayName}
+							Display name: {founder.destinyInfo.displayName}
 						</li>
 						<li>
-							Is Online: {props.Info.founder.isOnline.toString()}
+							Is Online: {founder.isOnline.toString()}
 						</li>
 					</ul>
 				</li>
