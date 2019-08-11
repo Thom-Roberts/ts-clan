1c22c305dd1fc648ae8ce6fec3ec1d44901cc5b4
Thomas Roberts <robertsorange@gmail.com>
2019-08-11 17:04:25 -0600

    Centered accordion elements

--- src/MemberList.tsx
+++ src/MemberList.tsx
@@ -73,48 +73,50 @@ export default class MemberList extends React.Component<{MemberList: MemberListP
 
 
       return (
-         <div style={{marginBottom: '10px', width: '600px',}}> {/* Space between each accordion */}
-            <div style={{paddingBottom: '5px',}}> {/* Space between the role name and bottom */}
-               <span style={{textAlign: 'left',}}>{MemberList[0].role}:</span>
-               <span style={{float: 'right',}}>{this.getPlayerOnlineCount().toString()} online</span>
-            </div>
-            <Accordion styled>
-               {MemberList.map((memberprops, index) => {
-                  return (
-                     <div key={`MemberList: ${memberprops.membershipId}`}>
-                        <Accordion.Title active={activeIndex === index} index={index} onClick={this.handleClick}
-                           style={{position: 'relative',}}>
-                           <Icon name='dropdown' />
-                           <span>
-                              {/* Platform image */}
-                              <img src={(function() {
-                                 switch(memberprops.membershipType) {
-                                    case 2:
-                                       return psnIcon;
-                                    case 3:
-                                       return xboxIcon;
-                                    case 4:
-                                       return pcIcon;
-                                    default:
-                                       throw new Error(`Invalid membership type: ${memberprops.membershipType}`);
-                                 }
-                              })()} alt="Temp" style={{width: '15px', height: '15px', marginRight: '5px', position: 'relative', top: '2px',}}/>
+         <div style={{marginBottom: '10px', width: '100%',}}> {/* Space between each accordion */}
+            <div style={{maxWidth: '600px', margin: '0 auto',}}>
+               <div style={{paddingBottom: '5px',}}> {/* Space between the role name and bottom */}
+                  <span style={{textAlign: 'left',}}>{MemberList[0].role}:</span>
+                  <span style={{float: 'right',}}>{this.getPlayerOnlineCount().toString()} online</span>
+               </div>
+               <Accordion styled>
+                  {MemberList.map((memberprops, index) => {
+                     return (
+                        <div key={`MemberList: ${memberprops.membershipId}`}>
+                           <Accordion.Title active={activeIndex === index} index={index} onClick={this.handleClick}
+                              style={{position: 'relative',}}>
+                              <Icon name='dropdown' />
+                              <span>
+                                 {/* Platform image */}
+                                 <img src={(function() {
+                                    switch(memberprops.membershipType) {
+                                       case 2:
+                                          return psnIcon;
+                                       case 3:
+                                          return xboxIcon;
+                                       case 4:
+                                          return pcIcon;
+                                       default:
+                                          throw new Error(`Invalid membership type: ${memberprops.membershipType}`);
+                                    }
+                                 })()} alt="Temp" style={{width: '15px', height: '15px', marginRight: '5px', position: 'relative', top: '2px',}}/>
 
-                              {memberprops.displayName}
-                              
-                           </span>
-                           <span style={{position: 'absolute', right: '10px',}}>{memberprops.onlineStatus ? ONLINESTATUS : OFFLINESTATUS}
-                           </span>
-                        </Accordion.Title>
-                        <Accordion.Content active={activeIndex === index}>
-                           <p>
-                              {memberprops.favoriteClass}: {memberprops.getStringForTimePlayed(memberprops.totalTimePlayed)}
-                           </p>
-                        </Accordion.Content>
-                     </div>
-                  );
-               })}
-            </Accordion>
+                                 {memberprops.displayName}
+                                 
+                              </span>
+                              <span style={{position: 'absolute', right: '10px',}}>{memberprops.onlineStatus ? ONLINESTATUS : OFFLINESTATUS}
+                              </span>
+                           </Accordion.Title>
+                           <Accordion.Content active={activeIndex === index}>
+                              <p>
+                                 {memberprops.favoriteClass}: {memberprops.getStringForTimePlayed(memberprops.totalTimePlayed)}
+                              </p>
+                           </Accordion.Content>
+                        </div>
+                     );
+                  })}
+               </Accordion>
+            </div>
          </div>
       );
    }
