ca1698d7021d04fee418dc09953fc587f79e7bdb
Thomas Roberts <robertsorange@gmail.com>
2019-08-18 20:24:55 -0600

    Commented out time played issues until I can resolve

--- src/MemberList.tsx
+++ src/MemberList.tsx
@@ -172,7 +172,10 @@ export default class MemberList extends React.Component<{MemberList: MemberListP
                            <Accordion.Content active={activeIndex === index}>
                                  <p>
                                     Favorite Class: {memberprops.favoriteClass}
-                                    <span style={{float: 'right',}}>{memberprops.getStringForTimePlayed(memberprops.favoriteClassTimePlayed * 60)} / {memberprops.getStringForTimePlayed(memberprops.totalTimePlayed)}</span>
+                                    {
+                                       // TODO: Fix this time issue, add a / for character time / total time
+                                    }
+                                    <span style={{float: 'right',}}>{/*memberprops.getStringForTimePlayed(memberprops.favoriteClassTimePlayed * 60)*/}{memberprops.getStringForTimePlayed(memberprops.totalTimePlayed)}</span>
                                  </p>
 
                               <Accordion styled>
